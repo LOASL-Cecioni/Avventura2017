3f7ac0a6f84cf7e8271e680f1aa45395dbe26183
MartinaCinelli27 <martinacinelli99@gmail.com>
2017-06-15 12:36:08 +0200

    inizio dell'implementazione del main e funzione per la schermata principale

--- Avventura2017.cpp
+++ Avventura2017.cpp
@@ -1,8 +1,18 @@
 #include "Avventura2017.h"
 #include <iostream>
-
+#include <string>
+using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv) 
+{
+	//la variabile command decide cosa fare dopo la visualizzazione della schermata principale
+	//la funzione start restituisce la scelta dell'utente
+	//se restituisce 1 si deve visualizzare il menu principale
+	//se restituisce 2 si deve visualizzare il manuale
+	//se restituisce 3 si inizia a giocare
+	int command;
+	command=start();
+	
 	return 0;
 }
---
+++ funzioneStart.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+//funzione chiamata all'inizio del gioco
+//ritorna 1 per aprire il menu
+//ritorna 2 per aprire il manuale
+//ritorna 3 per iniziare a giocare
+
+int start()
+{
+	string chosen;
+	cout<<"-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-"<<endl;
+	cout<<"-*-*-*-*-*-*-*-*-*-* AVVENTURA 2017 *-*-*-*-*-*-*-*-*-*-*-*-"<<endl;
+	cout<<"-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-"<<endl;
+	cout<<"                                                             "<<endl;
+	cout<<" benvenuto nel gioco AVVENTURA 2017! "<<endl;
+	cout<<"digita 'menu' per accedere al menu principale"<<endl;
+	cout<<"digita 'manuale' per accedere al manuale delle istruzioni"<<endl;
+	cout<<"digita 'gioca' per iniziare a giocare!"<<endl;
+	cin>>chosen;
+	if(chosen=="menu"|| chosen==1)
+	{
+		return 1;
+	} 
+	else if (chosen=="manuale"||chosen==2)
+	{
+		return 2;
+	}
+	else if (chosen=="gioca"||chosen==3)
+	{
+		return 3;
+	}
+	else 
+	{
+		cout<<"l'inserimento non è valido"<<endl;
+		chosen=start();
+	} 
+}
+	
+	
