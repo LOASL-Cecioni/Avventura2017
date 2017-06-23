db29efdd98db22f7a57cce4b30e4624957f18e56
MartinaCinelli27 <martinacinelli99@gmail.com>
2017-06-23 10:01:31 +0200

    ancora modifiche

--- Avventura2017.cpp
+++ Avventura2017.cpp
@@ -63,25 +63,22 @@ void guide()
 			cout<< "le stanze in totale sono 5"<<endl;
 			cout<< "BUONA FORTUNA!"<<endl;
 			cout<< "-------------------------------"<<endl;
-			cout<< "digita 0 per tornare al manuale"<<endl;
 		}
 		else if (choice==2)
 	    { 
 			cout<< "MOVIMENTI DEL GIOCATORE:"<<endl;
 			cout<<"I movimenti possibili sono:"<<endl;
-			cout<<"- cambia stanza"<<endl;
-			cout<<"- guarda un'altra parete"<<endl;
+			cout<<"		- cambia stanza"<<endl;
+			cout<<"		- guarda un'altra parete"<<endl;
 			cout<< "-------------------------------"<<endl;
-			cout<< "digita 0 per tornare al manuale"<<endl;
 		}
 		else if (choice==3)
 		{
 		    cout<< "INTERAZIONI CON OGGETTI:"<<endl;
-			cout<< "-puoi prendere un oggetto"<<endl;
-			cout<< "-puoi gettare un oggetto"<<endl;
-			cout<< "-puoi usare un oggetto su un altro per aprirlo o distruggerlo"<<endl;
+			cout<< "	-puoi prendere un oggetto"<<endl;
+			cout<< "	-puoi gettare un oggetto"<<endl;
+			cout<< "	-puoi usare un oggetto su un altro per aprirlo o distruggerlo"<<endl;
 			cout<< "-------------------------------------------------------------"<<endl;
-			cout<< "digita 0 per tornare al manuale"<<endl;	
 		}
 	}
 }
@@ -130,20 +127,14 @@ void menu()
 	{	
 		configuraStanzaDaComando();
 	}
-	else
-	{ 	
-		if(numeroScelto==2)
-		{	
-			cout<<"Carica vecchia partita"<<endl;
- 		}   
-		else 
-		{  
-			if(numeroScelto==3)	
-			{	
-				cout<<"Inizializzo una nuova partita"<<endl;      
-			}
-		}
-    }
+	else if(numeroScelto==2)
+	{	
+		cout<<"Carica vecchia partita"<<endl;
+ 	}   
+	else if(numeroScelto==3)	
+	{	
+		cout<<"Inizializzo una nuova partita"<<endl;      
+	}
 }
 
 //-------------------------------------------------------------------------------------------------
@@ -516,7 +507,191 @@ void CreateRoom()
 	livingroom.wall1.onWall[4].setEffect("la porta conduce a nuovi indizi");
 	
 	//----------------------------------------------TERZA STANZA-------------------------------------------------
+	Room kitchen;
+	kitchen.setRoomNumber(1);	//la cucina è la terza stanza
+	//--------------------PARETE NORD (1)------------------------------
+	//inizializzo il primo oggetto
+	//tavolo
+	kitchen.wall1.onWall[0].setObjectName("table");
+	kitchen.wall1.onWall[0].setRoomPosition(3);
+	kitchen.wall1.onWall[0].setWallPosition(1);
+	kitchen.wall1.onWall[0].setUsed("tenere panini");
+	kitchen.wall1.onWall[0].setEffect("contiene il foglio bianco se smontato");
+	//inizializzo il secondo oggetto
+	//foglio bianco
+	kitchen.wall1.onWall[1].setObjectName("foglio bianco");
+	kitchen.wall1.onWall[1].setRoomPosition(3);
+	kitchen.wall1.onWall[1].setWallPosition(1);
+	kitchen.wall1.onWall[1].setUsed("essere illuminato");
+	kitchen.wall1.onWall[1].setEffect("fornisce scritte se illuminate con luce uv");
+	//inizializzo il terzo oggetto
+	//panino
+	kitchen.wall1.onWall[2].setObjectName("panino");
+	kitchen.wall1.onWall[2].setRoomPosition(3);
+	kitchen.wall1.onWall[2].setWallPosition(1);
+	kitchen.wall1.onWall[2].setUsed("nasconde un foglio");
+	kitchen.wall1.onWall[2].setEffect("fornisce un foglio se lo si apre");
+	//inizializzo il quarto oggetto
+	//foglio con scritta
+	kitchen.wall1.onWall[3].setObjectName("foglio");
+	kitchen.wall1.onWall[3].setRoomPosition(3);
+	kitchen.wall1.onWall[3].setWallPosition(1);
+	kitchen.wall1.onWall[3].setUsed("si legge: l'orologio va un ora avanti'");
+	kitchen.wall1.onWall[3].setEffect("indirizza il giocatore verso l'orologio");
+	//inizializzo il quinto oggetto
+	//orologio
+	kitchen.wall1.onWall[4].setObjectName("orologio");
+	kitchen.wall1.onWall[4].setRoomPosition(3);
+	kitchen.wall1.onWall[4].setWallPosition(1);
+	kitchen.wall1.onWall[4].setUsed("ha una password sul retro");
+	kitchen.wall1.onWall[4].setEffect("fornisce la password se lo si gira per cambiare l'ora'");
+	//--------------------PARETE EST (2)------------------------------
+	//inizializzo il primo oggetto
+	//PASSWORD
+	kitchen.wall2.onWall[0].setObjectName("password");
+	kitchen.wall2.onWall[0].setRoomPosition(3);
+	kitchen.wall2.onWall[0].setWallPosition(2);
+	kitchen.wall2.onWall[0].setUsed("si usa sul frigo");
+	kitchen.wall2.onWall[0].setEffect("apre il frigo");
+	//inizializzo il secondo oggetto
+	//frigo
+	kitchen.wall2.onWall[1].setObjectName("frigo");
+	kitchen.wall2.onWall[1].setRoomPosition(3);
+	kitchen.wall2.onWall[1].setWallPosition(2);
+	kitchen.wall2.onWall[1].setUsed("contiene una bottiglia di champagne");
+	kitchen.wall2.onWall[1].setEffect("fornisce l'accendino se la si usa");
+	//inizializzo il terzo oggetto
+	//champagne
+	kitchen.wall2.onWall[2].setObjectName("bottiglia di champagne");
+	kitchen.wall2.onWall[2].setRoomPosition(3);
+	kitchen.wall2.onWall[2].setWallPosition(2);
+	kitchen.wall2.onWall[2].setUsed("ha un codice sul tappo");
+	kitchen.wall2.onWall[2].setEffect("fornisce il codice");
+	//inizializzo il quarto oggetto
+	//codice
+	kitchen.wall2.onWall[3].setObjectName("codice");
+	kitchen.wall2.onWall[3].setRoomPosition(3);
+	kitchen.wall2.onWall[3].setWallPosition(2);
+	kitchen.wall2.onWall[3].setUsed("apre la credenza");
+	kitchen.wall2.onWall[3].setEffect("la credenza è aperta");
+	//inizializzo il quinto oggetto
+	//credenza
+	kitchen.wall2.onWall[4].setObjectName("credenza");
+	kitchen.wall2.onWall[4].setRoomPosition(3);
+	kitchen.wall2.onWall[4].setWallPosition(2);
+	kitchen.wall2.onWall[4].setUsed("contiene un cacciavite e un foglio con scritta");
+	kitchen.wall2.onWall[4].setEffect("ottieni il cacciavite");
+	//--------------------PARETE SUD (3)------------------------------
+	//inizializzo il primo oggetto
+	//cacciavite
+	kitchen.wall3.onWall[0].setObjectName("cacciavite");
+	kitchen.wall3.onWall[0].setRoomPosition(3);
+	kitchen.wall3.onWall[0].setWallPosition(3);
+	kitchen.wall3.onWall[0].setUsed("si usa sul tavolo");
+	kitchen.wall3.onWall[0].setEffect("smonta il tavolo");
+	//inizializzo il secondo oggetto
+	//CASSAFORTE
+	kitchen.wall3.onWall[1].setObjectName("cassaforte");
+	kitchen.wall3.onWall[1].setRoomPosition(3);
+	kitchen.wall3.onWall[1].setWallPosition(3);
+	kitchen.wall3.onWall[1].setUsed("contiene la penna a luci uv");
+	kitchen.wall3.onWall[1].setEffect("hai la luce uv");
+	//inizializzo il terzo oggetto
+	//penna a luci uv
+	kitchen.wall3.onWall[2].setObjectName("penna a luci uv");
+	kitchen.wall3.onWall[2].setRoomPosition(3);
+	kitchen.wall3.onWall[2].setWallPosition(3);
+	kitchen.wall3.onWall[2].setUsed("fornisce luce uv"); 	
+	kitchen.wall3.onWall[2].setEffect("illumina le scritte invisibili");	
+	//inizializzo il quarto oggetto
+	//PORTA
+	kitchen.wall3.onWall[3].setObjectName("porta");
+	kitchen.wall3.onWall[3].setRoomPosition(3);
+	kitchen.wall3.onWall[3].setWallPosition(3);
+	kitchen.wall3.onWall[3].setUsed("porta alla stanza successiva");
+	kitchen.wall3.onWall[3].setEffect("fa passare di livello");
+	//inizializzo il quinto oggetto
+	//foglio con scritta
+	kitchen.wall4.onWall[0].setObjectName("foglio con scritta");
+	kitchen.wall4.onWall[0].setRoomPosition(3);
+	kitchen.wall4.onWall[0].setWallPosition(3);
+	kitchen.wall4.onWall[0].setUsed("ha una scritta:fa un po' caldo");
+	kitchen.wall4.onWall[0].setEffect("indirizza il giocatore verso il ventilatore");
 	
+	//--------------------PARETE OVEST (4)------------------------------
+	//inizializzo il primo oggetto
+	//
+	//lampadina
+	kitchen.wall3.onWall[4].setObjectName("lampadina");		//è un oggetto inutile
+	kitchen.wall3.onWall[4].setRoomPosition(3);	//serve per confondere
+	kitchen.wall3.onWall[4].setWallPosition(4);
+	kitchen.wall3.onWall[4].setUsed("non si usa");
+	kitchen.wall3.onWall[4].setEffect("non ha effetto");
+	
+	//inizializzo il secondo oggetto
+	//terza sedia
+	kitchen.wall4.onWall[1].setObjectName("sedia3");
+	kitchen.wall4.onWall[1].setRoomPosition(3);
+	kitchen.wall4.onWall[1].setWallPosition(4);
+	kitchen.wall4.onWall[1].setUsed("copre la presa di corrente");
+	kitchen.wall4.onWall[1].setEffect("il giocatore non vede la presa");
+	//inizializzo il terzo oggetto
+	//presa di corrente
+	kitchen.wall4.onWall[2].setObjectName("presa di corrente");
+	kitchen.wall4.onWall[2].setRoomPosition(3);
+	kitchen.wall4.onWall[2].setWallPosition(4);
+	kitchen.wall4.onWall[2].setUsed("fornisce corrente");
+	kitchen.wall4.onWall[2].setEffect("accende il ventilatore se collegato");
+	//inizializzo il quarto oggetto
+	//ventilatore
+	kitchen.wall4.onWall[4].setObjectName("ventilatore");
+	kitchen.wall4.onWall[4].setRoomPosition(3);
+	kitchen.wall4.onWall[4].setWallPosition(4);
+	kitchen.wall4.onWall[4].setUsed("essere acceso e girare");
+	kitchen.wall4.onWall[4].setEffect("girando fa muovere la tendina");
+	//inizializzo il quinto oggetto
+	//tendina
+	kitchen.wall4.onWall[4].setObjectName("tendina");
+	kitchen.wall4.onWall[4].setRoomPosition(3);
+	kitchen.wall4.onWall[4].setWallPosition(4);
+	kitchen.wall4.onWall[4].setUsed("nasconde il vaso");
+	kitchen.wall4.onWall[4].setEffect("il vaso è nascosto");
+	//--------------------PAVIMENTO (5)------------------------------
+	//inizializzo il primo oggetto
+	//vaso
+	kitchen.wall5.onWall[0].setObjectName("vaso");
+	kitchen.wall5.onWall[0].setRoomPosition(3);
+	kitchen.wall5.onWall[0].setWallPosition(5);
+	kitchen.wall5.onWall[0].setUsed("contiene il terriccio");
+	kitchen.wall5.onWall[0].setEffect("tiene il terriccio compatto");
+	//inizializzo il secondo oggetto
+	//terriccio
+	kitchen.wall5.onWall[1].setObjectName("terriccio");
+	kitchen.wall5.onWall[1].setRoomPosition(3);
+	kitchen.wall5.onWall[1].setWallPosition(5);
+	kitchen.wall5.onWall[1].setUsed("contiene la chiave per aprire la porta");
+	kitchen.wall5.onWall[1].setEffect("fornisce la chiave se dissotterrata");
+	//inizializzo il terzo oggetto
+	//quarta sedia
+	kitchen.wall5.onWall[2].setObjectName("sedia4");
+	kitchen.wall5.onWall[2].setRoomPosition(3);
+	kitchen.wall5.onWall[2].setWallPosition(5);
+	kitchen.wall5.onWall[2].setUsed("copre la serratura della porta");
+	kitchen.wall5.onWall[2].setEffect("il giocatore non vede la serratura");
+	//inizializzo il quarto oggetto
+	//CHIAVE
+	kitchen.wall5.onWall[3].setObjectName("chiave");
+	kitchen.wall5.onWall[3].setRoomPosition(3);
+	kitchen.wall5.onWall[3].setWallPosition(5);
+	kitchen.wall5.onWall[3].setUsed("si usa sulla porta");	//la porta si trova sulla parete sud(3)
+	kitchen.wall5.onWall[3].setEffect("apre la porta");
+	//inizializzo il quinto oggetto
+	//pizza
+	kitchen.wall5.onWall[4].setObjectName("pizza");		//è un oggetto inutile
+	kitchen.wall5.onWall[4].setRoomPosition(3);	//serve per confondere
+	kitchen.wall5.onWall[4].setWallPosition(5);
+	kitchen.wall5.onWall[4].setUsed("essere mangiata");
+	kitchen.wall5.onWall[4].setEffect("distrae il giocatore");
 	
 	//---------------------------------------------QUARTA STANZA-----------------------------------------------
 	Room studio;
@@ -710,16 +885,19 @@ void CreateRoom()
 int main(int argc, char** argv)
 {
 	//dichiarazione variabili
-	
+	int state=0;	//definisce lo stato in cui si trova il gioco
+	//state=0 quando si visualizza la schermata principale
 	
 	//la variabile command decide cosa fare dopo la visualizzazione della schermata principale
 	//la funzione start restituisce la scelta dell'utente
 	//se restituisce 1 si deve visualizzare il menu principale
 	//se restituisce 2 si deve visualizzare il manuale
 	//se restituisce 3 si inizia a giocare
-	int command;
-	command=start();
-	
+	if(state==0)
+	{
+		int command;
+		command=start();
+	}
 	//visualizzazione del menu
 	
 	if(command==1)
@@ -738,7 +916,12 @@ int main(int argc, char** argv)
 	
 	//INIZIO DEL GIOCO
 	//creazione del giocatore
-	createPlayer();
+	//quando state=1
+	if(state==1)
+	{
+		cout<<"*-*-*-*-*-*-*-*-*-*-*-*INIZIO DEL GIOCO*-*-*-*-*-*-*-*-*-*-*"<<endl;
+		createPlayer();
+	}
 	
 	//fine del gioco
 	//il gioco finisce quando si esce dalla stanza 5
